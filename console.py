3f4ff1e94ebc8a0c0eeb4eca6233228714673355
Diegoacosta127 <diegoacosta127@gmail.com>
2022-02-28 12:01:46 -0800

    task 6 almost done?

--- console.py
+++ console.py
@@ -1,2 +1,35 @@
 #!/usr/bin/python3
-"""
+"""console module doc"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = '(hbnb) '
+    file = None
+
+    def do_quit(self, arg):
+        """Quit command to exit the program\n"""
+        self.close()
+        return True
+
+    def do_EOF(self, arg):
+        """Quit command to exit the program"""
+        self.close()
+        return True
+
+    def close(self):
+        """Quit command to exit the program"""
+        if self.file:
+            self.file.close()
+            self.file = None
+
+    def emptyline(self):
+        """checks if there's no input for the prompt"""
+        if self.lastcmd:
+            self.lastcmd = ""
+            return self.onecmd('\n')
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
