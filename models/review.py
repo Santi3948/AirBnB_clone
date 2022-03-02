83c6dc1a89ed87a44253229a658d0fc556e6596f
Diegoacosta127 <diegoacosta127@gmail.com>
2022-03-02 06:41:44 -0800

    classes for task 9

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""review module doc"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class doc"""
+    palce_id = ""
+    user_id = ""
+    text = ""
