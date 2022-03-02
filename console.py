8a8832208f796737eb30187afe058b99d4ecde43
Santi3948 <santiagonegreyra@gmail.com>
2022-03-02 14:21:56 -0800

    work locally

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 import cmd
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.__init__ import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -15,17 +16,100 @@ class HBNBCommand(cmd.Cmd):
         if not arg:
             print("** class name missing **")
         else:
-            try:
-                var = arg()
-                with open()
-            except NameError:
+            if arg == "BaseModel":
+                var = eval(f'{arg}()')
+                var.save()
+                print(f'{var.id}')
+            else:
                 print("** class doesn't exist **")
+    
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        var = arg.split(" ")
+        if len(var) == 1 and var[0] == '':
+            print("** class name missing **")
+        else:
+            if var[0] != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(var) == 1:
+                print("** instance id missing **")
+            else:
+                my_dict = storage.all()
+                for obj in my_dict:
+                    var2 = obj.split(".")
+                    if var2[1] == var[1]:
+                        print(f'{my_dict[obj]}')
+                        return
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        var = arg.split(" ")
+        if len(var) == 1 and var[0] == '':
+            print("** class name missing **")
+        else:
+            if var[0] != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(var) == 1:
+                print("** instance id missing **")
+            else: 
+                my_dict = storage.all()
+                for obj in my_dict:
+                    var2 = obj.split(".")
+                    if var2[1] == var[1]:
+                        my_dict.pop(obj)
+                        return
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        lis = []
+        if arg:
+            if arg != "BaseModel":
+                print("** class doesn't exist **")
+            else:
+                my_dict = storage.all()
+                for obj in my_dict:
+                    var2 = obj.split(".")
+                    if var2[0] == arg:
+                        lis.append(str(my_dict[obj]))
+                    print(f'{lis}')
+        else:
+            my_dict = storage.all()
+            for obj in my_dict:
+                lis.append(str(my_dict[obj]))
+            print(f'{lis}')
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        spl = arg.split(" ")
+        if len(spl) == 1 and spl[0] == '':
+            print("** class name missing **")
+        else:
+            if spl[0] != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(spl) == 1:
+                print("** instance id missing **")
+            else:
+                flag = 0
+                my_dict = storage.all()
+                for obj in my_dict:
+                    var2 = obj.split(".")
+                    if var2[1] == spl[1]:
+                        flag = 1
+                if flag == 0:
+                    print("** no instance found **")
+                elif len(spl) == 2:
+                    print("** attribute name missing **")
+                elif len(spl) == 3:
+                    print("** value missing **")
+                else:
+                    
 
     def do_quit(self, arg):
         """Quit command to exit the program\n"""
         self.close()
         return True
-
     def do_EOF(self, arg):
         """Quit command to exit the program"""
         self.close()
