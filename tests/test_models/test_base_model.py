a83ac27d3c9bde2071cae100ed998360b21f48ba
Diegoacosta127 <diegoacosta127@gmail.com>
2022-03-03 13:54:56 -0800

    tests for BaseModel almost done

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 """unittest base model doc"""
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
 
-class test_base(unittest.TestCase):
+class test_base_model(unittest.TestCase):
     """test_base doc"""
+    def test_docstring(self):
+        """test for docstrings"""
+        self.assertIsNotNone(BaseModel.__doc__)
+
     def test_base(self):
         """test_base doc"""
-        # code test here!
-
-    def test_str(self):
-        """test_str doc"""
         bm = BaseModel()
-        self.assertEqual(bm.__str__, f'[{self.__class__.__name__}] \
-({self.id}) {self.__dict__}')
+        self.assertEqual(isinstance(bm, BaseModel), True)
+        self.assertEqual(isinstance(bm.id, str), True)
+        self.assertEqual(type(bm.created_at), datetime)
+        self.assertIsInstance(bm.updated_at, datetime)
+        self.assertEqual(str(bm), f"[{bm.__class__.__name__}] \
+({bm.id}) {bm.__dict__}")
+        bm.save()
+        self.assertNotEqual(bm.created_at, bm.updated_at)
+        test_dict = bm.to_dict()
+        self.assertEqual(type(test_dict), dict)
+        self.assertEqual(test_dict, bm.to_dict())
+
 
 if __name__ == '__main__':
     unittest.main()
